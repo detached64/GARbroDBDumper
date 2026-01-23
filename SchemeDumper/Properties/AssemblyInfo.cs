6e6b57ba1ba896a16327c1715e1fe021b0b1c6cf
detached64 <169419571+detached64@users.noreply.github.com>
2026-01-23 17:06:40 +0800

    Initial commit

---
+++ SchemeDumper/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SchemeDumper")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("detached64")]
+[assembly: AssemblyProduct("SchemeDumper")]
+[assembly: AssemblyCopyright("Copyright © detached64 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2747e530-a311-4e66-a54a-471d49e1ef0d")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
